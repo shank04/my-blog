5fbf8fac406f07097a81636c2cdc28c5267b0904
shashank <shashankjha14@gmail.com>
2016-07-25 15:02:50 +0530

    commit 2

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Post
 
-# Create your views here.
+def post_list(request):
+    posts = Post.objects.all
+    return render(request, 'blog/post_list.html', {'posts': posts})
