2359fa257ec5c67246e6f87c3c1c0dd5a9f2b18a
shashank <shashankjha14@gmail.com>
2016-07-29 02:43:26 +0530

    fixed a glitch

--- mysite/settings.py
+++ mysite/settings.py
@@ -31,8 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    #'markdown_deux',
-    'django_markdown',    
+    
+        
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
